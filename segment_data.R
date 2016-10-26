091e23350127071735a0a5e1ac6c2a2ae47b5af8
Tyler Romero <tyler.alexander.romero@gmail.com>
2016-10-25 17:37:15 -0700

    Changed fwrite to write.csv

--- segment_data.R
+++ segment_data.R
@@ -1,5 +1,3 @@
-# Install development version of data.table - REQUIRED for fwrite
-install.packages("data.table", repos = "https://Rdatatable.github.io/data.table", type = "source")
 library(data.table)
 
 input_directory <- "C:\\Users\\Tyler\\Documents\\My Documents\\MS&E226\\outbrain\\input"
@@ -14,5 +12,5 @@ train.ind = sample(nrow(clicks_train), 4*round(nrow(clicks_train)/5)) #80% of da
 clicks_train.train = clicks_train[train.ind,]
 clicks_train.test = clicks_train[-train.ind,]
 
-fwrite(clicks_train.train, "training_clicks.csv")
-fwrite(clicks_train.test, "testing_clicks.csv")
+write.csv(clicks_train.train, file = "training_click.csv")
+write.csv(clicks_train.test, file = "testing_clicks.csv")
