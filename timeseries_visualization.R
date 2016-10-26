136132d7960f105f78aff9a242900875a02a2e6f
Tyler Romero <tyler.alexander.romero@gmail.com>
2016-10-25 23:23:43 -0700

    Added timeseries_visualization.R

---
+++ timeseries_visualization.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(ggplot2)
+library(dplyr)
+
+# TODO: Directory should not be hardcoded! Rather, will define a folder structure in Readme.md and put data in a local folder
+input_directory <- "C:\\Users\\Tyler\\Documents\\My Documents\\MS&E226\\outbrain\\input"
+current_directory <- dirname(parent.frame(2)$ofile)
+setwd(input_directory)
+
+page_views <- fread("page_views_sample.csv")
+events <- fread("events.csv")
+colnames(page_views)[[3]] <- "loadTimestamp"
+colnames(events)[[4]] <- "eventTimestamp"
+
+view_info <- merge(page_views, events, by = c("uuid", "document_id", "geo_location", "platform"))
+view_info <- mutate(view_info, timeOnPage = eventTimestamp - loadTimestamp)
+
+hist1 <- ggplot(data=view_info, aes(view_info$timeOnPage)) + 
+  geom_histogram()
+                 
+
