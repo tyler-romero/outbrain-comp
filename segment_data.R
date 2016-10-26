91f63ed71835c4c4fa9feaa67b18a84b0ea2b6f8
Tyler Romero <tyler.alexander.romero@gmail.com>
2016-10-25 18:09:02 -0700

    Update segment_data.R

--- segment_data.R
+++ segment_data.R
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# Install development version of data.table - REQUIRED for fwrite
-# TODO: Put check for already being installed
-install.packages("data.table", repos = "https://Rdatatable.github.io/data.table", type = "source")
-=======
->>>>>>> dev
 library(data.table)
 
 # TODO: Directory should not be hardcoded! Rather, will define a folder structure in Readme.md and put data in a local folder
@@ -19,10 +13,5 @@ train.ind = sample(nrow(clicks_train), 4*round(nrow(clicks_train)/5)) #80% of da
 clicks_train.train = clicks_train[train.ind,]
 clicks_train.test = clicks_train[-train.ind,]
 
-<<<<<<< HEAD
-fwrite(clicks_train.train, "training_clicks.csv")
-fwrite(clicks_train.test, "testing_clicks.csv")
-=======
 write.csv(clicks_train.train, file = "training_click.csv")
 write.csv(clicks_train.test, file = "testing_clicks.csv")
->>>>>>> dev
