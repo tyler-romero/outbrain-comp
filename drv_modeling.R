d8e0285e7f8c0076fa0c93e4e9d1df2090d4f655
Tyler Romero <tyler.alexander.romero@gmail.com>
2016-11-17 15:48:30 -0800

    DRV ROC implementation

--- drv_modeling.R
+++ drv_modeling.R
@@ -8,6 +8,13 @@ library(MLmetrics)
 #input_directory <- ".\\input"
 #setwd(input_directory)
 
+#----------- Helper Functions ------------
+#Prediction Threshold for naive bayes
+predictThresh <- function(prediction, K) {
+  df <- as.data.frame(prediction)
+  return(as.numeric(df[2]/df[1] > K))
+}
+#-----------------------------------------
 
 
 brv <- fread("brv.csv")
@@ -20,23 +27,71 @@ train.ind = sample(nrow(brv), 4*round(nrow(brv)/5)) #80% of data is for training
 brv.train = brv[train.ind,]
 brv.test = brv[-train.ind,]
 
-f1 <- factor(clicked) ~ display_id + ad_id + document_id + platform + geo_location +
-  traffic_source + factor(weekDay) + hour + topic_id*topic_confidence_level +
-  source_id + publisher_id + category_id*category_confidence_level
+f1 <- factor(clicked) ~ platform + geo_location + traffic_source + factor(weekDay) + loadTimestamp +
+  display_id + document_id + ad_id
 
 
-binomial.glm1 <- glm(f1, data = brv.train, family = binomial())
+#binomial.glm1 <- glm(f1, data = brv.train, family = binomial())  #Logisitic Regression
 
 
-predictThresh <- function(prediction, K) {
-  df <- as.data.frame(prediction)
-  return(as.numeric(df[2]/df[1] > K))
+#naiveBayes.m1 <- naiveBayes(f1, data=brv.train)
+#pre1 <- predictThresh(predict(naiveBayes.m1, brv.train, type = "raw"), K=4)
+#print(ZeroOneLoss(pre1, brv.train$clicked))
+
+naivebayesROC <- function() {
+  naiveBayes.m1 <- naiveBayes(f1, data=brv.train)
+  rawPre <- predict(naiveBayes.m1, brv.train, type = "raw")
+  bayes.roc <- data.frame()
+  
+  for(i in seq(0, 1, 0.01)){
+    pre1 <- predictThresh(rawPre, K=i)
+    cm <- as.matrix(ConfusionMatrix(y_pred = pre1, y_true = brv.train$clicked))
+    if(!("0" %in% colnames(cm))) cm <- cbind(c(0,0),cm)
+    else if(!("1" %in% colnames(cm))) cm <- cbind(cm, c(0,0))
+    print(cm)
+    tn <- cm[1]
+    fn <- cm[2]
+    fp <- cm[3]
+    tp <- cm[4]
+    tpr <- tp/(tp+fn)
+    fpr <- fp/(fp+tn)
+    zeroOne <- ZeroOneLoss(pre1, brv.train$clicked)
+    newRow <- data.frame(thresh=i, tn=tn, fp=fp, fn=fn, tp=tp, tpr=tpr, fpr=fpr, zo=zeroOne)
+    
+    bayes.roc <- bind_rows(bayes.roc, newRow)
+  }
+  return(bayes.roc)
 }
-naiveBayes.m1 <- naiveBayes(f1, data=brv.train)
-pre <- predictThresh(predict(naiveBayes.m1, brv.train, type = "raw"), K=4)
-print(ZeroOneLoss(pre, brv.train$clicked))
+roc <- naivebayesROC()
+roc.plot <- ggplot(roc, aes(fpr, tpr)) + geom_point() + xlim(0,1) + ylim(0,1) +geom_abline(slope=1, intercept=0, color="blue")
+print(roc.plot)
+
+#svm.m1 <- svm(f1, data=brv.train, class.weights = c("0"=1, "1"=4))
+#pre2 <- predict(svm.m1, brv.train)
+#print(ZeroOneLoss(pre2, brv.train$clicked))
+
+svmROC <- function() {
+  bayes.roc <- data.frame()
+  
+  for(i in seq(0,0.5,0.01)){
+    svm.m1 <- svm(f1, data=brv.train, class.weights = c("0"=1, "1"=i))
+    pre1 <- predict(svm.m1, brv.train)
+    cm <- ConfusionMatrix(y_pred = pre1, y_true = brv.train$clicked)
+    print(cm)
+    tn <- cm[1]
+    fp <- cm[2]
+    fn <- cm[3]
+    tp <- cm[4]
+    tpr <- tp/(tp+fn)
+    fpr <- fp/(fp+tn)
+    zeroOne <- ZeroOneLoss(pre1, brv.train$clicked)
+    newRow <- data.frame(thresh=i, tn=tn, fp=fp, fn=fn, tp=tp, tpr=tpr, fpr=fpr, zo=zeroOne)
+    
+    bayes.roc <- bind_rows(bayes.roc, newRow)
+  }
+  return(bayes.roc)
+}
+  
 
 
-svm.m1 <- svm(f1, data=brv.train, class.weights = c("0"=0.2, "1"=0.8))
-pre <- predict(svm.m1, brv.train)
 
